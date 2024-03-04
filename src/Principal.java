af54352f41ed9e2d86efec7a37731bbb698c79df
nicolasdca <nicolasdavid193@gmail.com>
2024-03-04 19:23:27 -0300

    Aula 4

--- src/Principal.java
+++ src/Principal.java
@@ -1,21 +1,54 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
 
     public static void main(String[] args) {
         Filme favorito = new Filme();
-
         favorito.setNome("The Matrix");
         favorito.setAnoDeLancamento(1999);
         favorito.setDuracaoEmMinutos(135);
         favorito.setIncluidoNoPlano(true);
-
         favorito.exibeFichaTecnica();
         favorito.avalia(9);
         favorito.avalia(8);
         favorito.avalia(9);
 
         System.out.println("Média de avaliações do filme: " + favorito.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(45);
+        lost.setAtiva(false);
+        System.out.println("Duração para maratonar Lost: " + lost.getDuracaoEmMinutos());
+
+        Filme avatar = new Filme();
+        avatar.setNome("Avatar");
+        avatar.setAnoDeLancamento(2012);
+        avatar.setDuracaoEmMinutos(180);
+        avatar.setIncluidoNoPlano(true);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(favorito);
+        calculadora.inclui(avatar);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(favorito);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(lost);
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
     }
 
 }
