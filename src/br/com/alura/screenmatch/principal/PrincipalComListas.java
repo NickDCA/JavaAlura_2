62462ceed09147f074edd52e37dc3fb734ce2566
nicolasdca <nicolasdavid193@gmail.com>
2024-03-06 18:55:08 -0300

    Aula 3 Collections

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,32 @@
+package br.com.alura.screenmatch.principal;
+
+import java.util.ArrayList;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+        Filme favorito = new Filme("The Matrix", 1999);
+        favorito.avalia(9);
+        Filme avatar = new Filme("Avatar", 2012);
+        avatar.avalia(6);
+        var filmeDoPaulo = new Filme("Dogville", 2003);
+        filmeDoPaulo.avalia(10);
+        Serie lost = new Serie("Lost", 2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(avatar);
+        lista.add(favorito);
+        lista.add(filmeDoPaulo);
+        lista.add(lost);
+
+        for (Titulo item : lista) {
+            System.out.println(item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() > 2) {
+                System.out.println("Classificação: " + filme.getClassificacao());
+            }
+        }
+    }
+}
