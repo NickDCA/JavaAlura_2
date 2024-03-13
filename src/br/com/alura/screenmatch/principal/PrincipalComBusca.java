9464af34d23847293e521f267340af90615bffaf
nicolasdca <nicolasdavid193@gmail.com>
2024-03-13 11:40:24 -0300

    APIs Aula 3

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -11,38 +11,51 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import br.com.alura.screenmatch.excecao.ErroDeConversaoDeAnoException;
 import br.com.alura.screenmatch.modelos.Titulo;
 import br.com.alura.screenmatch.modelos.TituloOmdb;
 
 public class PrincipalComBusca {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        Scanner leitura = new Scanner(System.in);
-        System.out.println("Digite um filme para busca: ");
-        var busca = leitura.nextLine();
+        public static void main(String[] args) throws IOException, InterruptedException {
+                Scanner leitura = new Scanner(System.in);
+                System.out.println("Digite um filme para busca: ");
+                var busca = leitura.nextLine();
 
-        String endereco = "http://www.omdbapi.com/?t=" + busca + "&apikey=c2ab0658";
+                String endereco = "http://www.omdbapi.com/?t=" + busca.replace(" ", "+") + "&apikey=c2ab0658";
 
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(endereco))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
+                try {
+                        HttpClient client = HttpClient.newHttpClient();
+                        HttpRequest request = HttpRequest.newBuilder()
+                                        .uri(URI.create(endereco))
+                                        .build();
+                        HttpResponse<String> response = client
+                                        .send(request, HttpResponse.BodyHandlers.ofString());
 
-        String json = response.body();
-        System.out.println(json);
+                        String json = response.body();
+                        System.out.println(json);
 
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
+                        Gson gson = new GsonBuilder()
+                                        .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                                        .create();
 
-        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(meuTituloOmdb);
+                        TituloOmdb meuTituloOmdb = gson.fromJson(json, TituloOmdb.class);
+                        System.out.println(meuTituloOmdb);
 
-        Titulo meuTitulo = new Titulo(meuTituloOmdb);
-        System.out.println("Titulo já convertido");
-        System.out.println(meuTitulo);
+                        Titulo meuTitulo = new Titulo(meuTituloOmdb);
+                        System.out.println("Titulo já convertido");
+                        System.out.println(meuTitulo);
+                } catch (NumberFormatException e) {
+                        System.out.println("Aconteceu um erro: ");
+                        System.out.println(e.getMessage());
+                } catch (IllegalArgumentException e) {
+                        System.out.println("Algum erro de argumento na busca, verigique o endereço");
+                        System.out.println(e.getMessage());
+                } catch (ErroDeConversaoDeAnoException e) {
+                        System.out.println(e.getMessage());
+                }
 
-        leitura.close();
-    }
+                System.out.println("Programa finalizou corretamente");
+
+                leitura.close();
+        }
 }
